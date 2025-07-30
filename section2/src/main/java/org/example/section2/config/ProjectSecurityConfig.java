7896897ff89932107a9671daca1016b29eecf4f0
mohamedsayedms4 <mohamedms4002@gmail.com>
2025-07-30 19:28:42 +0300

    lec2

---
+++ section2/src/main/java/org/example/section2/config/ProjectSecurityConfig.java
@@ -0,0 +1,32 @@
+package org.example.section2.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+//        http.authorizeHttpRequests((authorizeRequests) ->
+//                authorizeRequests.anyRequest().denyAll());
+
+        http.authorizeHttpRequests(authorizeRequests ->{
+            authorizeRequests.requestMatchers
+                    ("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                    .requestMatchers("/notices", "/contact", "/error").permitAll();
+        });
+        http.formLogin(form -> form.disable());
+        http.httpBasic(httpBasic -> httpBasic.disable());
+
+
+
+        return http.build();
+    }
+}
